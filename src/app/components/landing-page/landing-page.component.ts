2984fa56b888fe438a9638820f3681515b3e48ac
raguldavid1999 <raguldavid1999@gmail.com>
2024-06-04 01:08:51 +0530

    Home banner design

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [CarouselModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
 })
-export class LandingPageComponent {}
+export class LandingPageComponent {
+  customOptions: OwlOptions = {
+    loop: true,
+    items: 1,
+    dots: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    nav: false,
+    autoplay: true,
+  };
+}
